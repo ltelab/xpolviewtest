971f11c9be57721b17bac080f1069df91c885fb0
Polar X Bear <radarop@epfl>
2020-09-25 13:11:29 +0000

    updated gitgnore

--- .ipynb_checkpoints/xpolviewtest-checkpoint.sh
+++ .ipynb_checkpoints/xpolviewtest-checkpoint.sh
@@ -2,9 +2,6 @@
 while true
 do
 git pull
-git rm /users/radarop/xpolviewtest/latest.png
-git commit -a --allow-empty-message -m ''
-git push
 python3 find_latest_quicklook.py
 git add .
 git commit -a --allow-empty-message -m ''
